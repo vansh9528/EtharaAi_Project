6d0b77af21a0bfcb29f6af7c6e84fcd6d2fa59eb
vansh9528 <vansh.choudhary238@gmail.com>
2026-05-10 20:52:39 +0530

    updating backend

--- Ethara-Ai-Dashboard/src/lib/api.ts
+++ Ethara-Ai-Dashboard/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "https://etharaai-project.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
